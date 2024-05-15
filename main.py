0c2b4924b77d8eff7541d7485ca30e1a488698de
Abanoub-Samy <21p0356@eng.asu.edu.eg>
2024-05-15 12:05:41 +0300

    Added GeckoDriver to follium, now working safely

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 #from IPython.display import Image
 
 import folium
+import time
 
 from geopy.geocoders import Nominatim
 
 import asyncio
 from pyppeteer import launch
 
-#from selenium import webdriver
-
+from selenium import webdriver
+import os
 
 
 # Initialize Nominatim geocoder
@@ -20,14 +21,14 @@ address = "1600 Pennsylvania Avenue NW, Washington, D.C."
 # Get the coordinates
 location = geolocator.geocode(address)
 
-# Print latitude and longitude
+# Print latitude and longitude   test
 print("Latitude:", location.latitude)
 print("Longitude:", location.longitude)
 
 #Image(r"C:\Users\Bebo\Desktop\Artificial_Intelligence\Project\testing.png").save("output.png")
 
 # Create a map centered at a specific location
-mymap = folium.Map(location=[location.latitude ,location.longitude], zoom_start=1000)
+mymap = folium.Map(location=[location.latitude ,location.longitude], zoom_start=100)
 
 # Add a satellite imagery layer (you can replace the URL with other sources)
 folium.TileLayer(
@@ -42,27 +43,32 @@ folium.TileLayer(
 mymap.save('mymap.html')
 
 
-'''    SELENIUM IS NOT RUNNING RIGHT NOW, TRYING ALTERNATIVES
 
-# Set up the Chrome driver (you can use other browsers too)
+# Set up the Firefox driver (you can use other browsers too)
+
 try:
-    # Initialize the Chrome WebDriver
-    driver = webdriver.Chrome()
+    # Get the current directory where the script is located
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    # Specify the relative path to mymap.html
+    relative_path = os.path.join(current_directory, 'mymap.html')
+    driver = webdriver.Firefox()
+    driver.get('file://' + relative_path)  # Use 'file://' prefix for local file paths
     print("WebDriver is running successfully!")
 except Exception as e:
     print(f"Error: {e}")
 
-# Navigate to your HTML file (replace 'file:///path/to/mymap.html' with the actual file path)
-relative_path = r'mymap.html'
-driver.get(relative_path)
+time.sleep(2)
 # Capture the screenshot and save it as mymap.png
 driver.save_screenshot('mymap.png')
 
 # Close the browser window
 driver.quit()
 
-'''
-async def main():
+
+
+
+#pypeteer as a backup for follium, but not needed since firefox gecko is working fine
+'''async def main():
     browser = await launch()
     page = await browser.newPage()
 
@@ -75,3 +81,4 @@ async def main():
     await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
+'''
